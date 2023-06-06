6c1221ebc47c39ebec7ed6a2e0321d5cc82310de
David Brinovec <david.brinove@gmail.com>
2023-06-06 16:03:34 -0400

    convenience updates

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ docker run -it --name zmk zmk
 docker cp zmk:/app/firmware/ ./
 docker stop zmk
 docker rm zmk
+find ./firmware | tail -2 | paste - - | awk '{system("ln -sf "$1" latest-left.uf2; ln -sf "$2" latest-right.uf2;")}'
